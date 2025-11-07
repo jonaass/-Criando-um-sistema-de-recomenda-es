7750dc6c1a994050997bf773b542923541388746
jonaass <jonascn.2021@gmail.com>
2025-11-07 20:29:31 -0300

    calculo_euclidiana

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import math
 
 avaliacoes = {
     'Jonas': {'Matrix': 4.5, 'Avatar': 3.0, 'Titanic': 4.0, 'Interestelar': 5.0},
@@ -6,3 +7,19 @@ avaliacoes = {
     'Fernanda': {'Avatar': 4.5, 'Titanic': 3.0, 'Interestelar': 4.5, 'Duna': 4.0}
 }
 
+print(avaliacoes['Jonas']['Matrix'])  
+
+def distancia_euclidiana(usuario1, usuario2, avaliacoes):
+    filmes_em_comum = []
+    for filme in avaliacoes[usuario1]:
+        if filme in avaliacoes[usuario2]:
+            filmes_em_comum.append(filme)
+    if len(filmes_em_comum) == 0:
+        return None
+    soma = 0
+    for filme in filmes_em_comum:
+        soma += (avaliacoes[usuario1][filme] - avaliacoes[usuario2][filme]) ** 2
+    return math.sqrt(soma)
+
+print(distancia_euclidiana('Jonas', 'Tony', avaliacoes))
+print(distancia_euclidiana('Jonas', 'Sophia', avaliacoes))
